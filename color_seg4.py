49eadda1423babdd875533c8e2e30b8cbc524309
Lethe <1531046423@qq.com>
2024-04-07 21:18:33 +0800

    color4

--- color_seg4.py
+++ color_seg4.py
@@ -42,16 +42,16 @@ def process_image(image_path, num_top_colors=6, eps=10):
         cv2.imwrite(f'output_image{idx}.jpg', mask)
         print(f'cnt:{np.sum(mask)}')
 
-@timedecorator
+# @timedecorator
 def process_image2(image, num_top_colors=6, eps=10):
 
     height, width, _ = image.shape
 
     # 将图像转换为一维数组
     flattened_image = image.reshape(-1, 3)
-    print(flattened_image.shape)
+    # print(flattened_image.shape)
 
-    print(Counter(map(tuple, flattened_image)))
+    # print(Counter(map(tuple, flattened_image)))
     # 计算最常见的颜色
     colors_set_top_n = Counter(map(tuple, flattened_image)).most_common(num_top_colors)
     pic_result_list = []
@@ -65,21 +65,28 @@ def process_image2(image, num_top_colors=6, eps=10):
 
         # 创建掩码
         mask = np.where(total_diff < eps, 255, 0).reshape(height, width).astype(np.uint8)
+        # mask = np.where(total_diff < eps, 255, 0).reshape(height, width)
         pic_result_list.append(mask)
         # 保存掩码
         cv2.imwrite(f'output_image{idx}.jpg', mask)
         print(f'cnt:{np.sum(mask)}')
+    print("pic_result_list:", pic_result_list)
     return pic_result_list
 
 def get_img(img):
-    print(img)
-    print(type(img))
-    res = process_image2(img)
-    return res
+    # print(img)
+    # print(type(img))
+    mask_res = process_image2(img)
+    print("mask_res:", mask_res)
+    print(len(mask_res))
+    return img
 
 if __name__ == "__main__":
-    
-    # demo = gr.Interface(get_img, gr.Image(), "image")
-    demo = gr.Interface(get_img, gr.Image(), gr.Gallery())
-    demo.launch(debug=True)
-    # process_image('pic3.png')
+    with gr.Blocks() as demo:
+        with gr.Column():
+            gr.Interface(process_image2, 
+                         [gr.Image(),
+                            gr.Slider(1,10,step=1, label="图片颜色种类", interactive=True),
+                            gr.Slider(0,30,step=1, label="RGB颜色误差", interactive=True)], 
+                        outputs=gr.Gallery(label="生成结果：",interactive=True,allow_preview=True))
+    demo.launch()
